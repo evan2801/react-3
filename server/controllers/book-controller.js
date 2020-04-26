934004f0258d97b6ded0d2fa447d87b71b55a90e
evan stein <evanstein28@gmail.com>
2020-04-26 19:49:31 -0400

    addin server side

---
+++ server/controllers/book-controller.js
@@ -0,0 +1,24 @@
+// import book model
+const { Book } = require("../models");
+
+module.exports = {
+ async getAllBooks(req, res) {
+     const books = await Book.find();
+
+     return res.json(books);
+ }, 
+ async saveBook(req,res) {
+     const savedBook = await Book.create(req.body);
+     return res.json(savedBook);
+},
+ async deleteBook(req, res) {
+     const deletedBook = await Book.findOneAndRemove({_id: req.params.id});
+     
+     if (!deletedBook) {
+        return res.status(404).json({message: "Couldnt find book with this id!"});
+     }
+
+     return res.json(deletedBook);
+ },  
+};
+
